5a875ead3bfa513612d23106b03e3330c9d7bbc7
Katrina Pritchard <pritckat@gmail.com>
2018-09-28 19:40:07 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if params[:name] == nil
+    if params[:name] == nil || params[:name].strip == ""
       redirect_to '/login'
     else
     session[:username] = params[:username]
